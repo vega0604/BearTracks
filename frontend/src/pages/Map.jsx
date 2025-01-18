991c117e429f607acd11407b9964c2e8a24017c6
Aleksandar Bursac <aleksbrsc@gmail.com>
2025-01-18 17:13:18 -0500

    style(Map.jsx): sidebar setup

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import styles from '@css/map.module.css';
 import MapComponent from "./MapApi";
-import styles from './Map.module.css';
+import light_paw from '@assets/logos/light_paw.svg';
+import search_icon from '@assets/icons/search.svg';
 
 function Map() {
     return (
-        <div>
-            <div className={styles.nav_container}>
-                <nav>
-
-                </nav>
+        <section id={styles.map_section}>
+            <nav id={styles.nav_container}>
+                <div id={styles.header}>
+                    <div id={styles.logo}>
+                        <img src={light_paw} alt="Paw icon"/>
+                        <h1>BearTracks</h1>
+                    </div>
+                    <div id={styles.campus}>
+                        <h2>Trafalgar 🍯</h2>
+                    </div>
+                </div>
+                <div id={styles.search_bar}>
+                    <input placeholder="Search..."/>
+                    <img src={search_icon} alt="Search icon"/>
+                </div>
+            </nav>
+            <div id={styles.map_wrapper}>
+                <div id={styles.map_container}>
+                    <MapComponent/>
+                </div>
             </div>
-            <div className={styles.map_container}>
-                {/* PUT MAP COMPONENT HERE! */}
-                <MapComponent/>
-            </div>
-        </div>
+        </section>
     );
 }
 
