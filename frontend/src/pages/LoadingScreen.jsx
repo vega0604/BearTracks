b841c67a855fe80910f6e3a5980d6bff7d70d24e
Aleksandar Bursac <aleksbrsc@gmail.com>
2025-02-12 19:52:27 -0500

    perf(LoadingScreen.jsx): using svg instead of font in loading screen

--- frontend/src/pages/LoadingScreen.jsx
+++ frontend/src/pages/LoadingScreen.jsx
@@ -1,13 +1,15 @@
 import styles from '@css/loading.module.css';
-import light_paw from '@assets/logos/light_paw.svg';
+// import light_paw from '@assets/logos/light_paw.svg';
+import light_logo_full from '@assets/logos/light_logo_full.svg';
 
 const LoadingScreen = () => {
     return (
         <div id={styles.loading_screen}>
             <div id={styles.loading_container}>
                 <div id={styles.loading_logo} >
-                    <img src={light_paw} alt="Paw icon"/>
-                    <h1>BearTracks</h1>
+                    <img src={light_logo_full}/>
+                    {/* <img src={light_paw} alt="Paw icon"/> */}
+                    {/* <h1>BearTracks</h1> */}
                 </div>
                 <div id={styles.loading_spinner}></div>
                 <div className={styles.lds_default}>
