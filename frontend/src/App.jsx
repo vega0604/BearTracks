efdab77d9c8bc6a1181b364c66f63d0a09524a7b
Aleksandar Bursac <aleksbrsc@gmail.com>
2025-02-25 23:27:04 -0500

    feat: vercel analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from '@pages/Landing';
 import Map from '@pages/Map';
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Map />}/>
-        <Route path='/map' element={<Map />}/>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Map />}/>
+          <Route path='/map' element={<Map />}/>
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   )
 }
 
